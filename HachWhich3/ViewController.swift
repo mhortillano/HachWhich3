fad6a27725602cebdd932b42b8292eb2a788c598
mhortillano <mhortillano@github.com>
2018-02-05 10:38:06 -1000

    Program change label to "About".

--- HachWhich3/ViewController.swift
+++ HachWhich3/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstLabel: UILabel!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+   self.firstLabel.text = "About"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+  
 }
 
